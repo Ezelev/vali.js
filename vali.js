b3e0bed5c415fd1818e08a790b28e44693134d1b
Ilya Ezelev <iiezelev@gmail.com>
2020-07-31 17:43:17 +0300

    Update vali.js

--- vali.js
+++ vali.js
@@ -8,13 +8,28 @@ const errorsHolder = document.getElementById("errors-holder");
 
 form.addEventListener("submit", (e) => {
 
+
+    
+    let requiredFields = ["name","email"];
+    var inputs = form.elements;
+    var inputByIndex = inputs[0];
+    var inputByName = inputs["username"];
+
     let messages = [];
-    if(name.value === "" || name.value == null) {
-        messages.push("Field is required!");
+    for (var i = 0; i < inputs.length; i++) {
+        let currentInput = inputs.item(i);
+        if(requiredFields.includes(currentInput.id)) {
+            console.log("Field with ID: " + currentInput.id + " is REQUIERED");
+            if(currentInput.value === "" || currentInput.value == null) {
+                messages.push("Field is required!");
+            }
+        }
     }
 
     if(messages.length > 0) {
         e.preventDefault();
-        errorsHolder.innerHTML = messages.join(", ");
+        errorsHolder.innerHTML = messages.join("<br>");
     }
+
+    e.preventDefault();
 });
